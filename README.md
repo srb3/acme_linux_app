eb57fd7af726d546f7d5a72d85530c941357919d
srb3 <sbrown@chef.io>
2020-03-11 19:11:42 +0000

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# acme_linux_app
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,16 @@
+name 'acme_linux_app'
+maintainer 'Steve Brown'
+maintainer_email 'sbrown@chef.io'
+license 'Apache-2.0'
+description 'Installs/Configures acme_linux_app'
+long_description 'Installs/Configures acme_linux_app'
+version '0.1.0'
+chef_version '>= 14.0'
+
+%w(centos).each do |os|
+  supports os
+end
+
+
+issues_url 'https://github.com/srb3/acme_linux_app/issues'
+source_url 'https://github.com/srb3/acme_linux_app'
